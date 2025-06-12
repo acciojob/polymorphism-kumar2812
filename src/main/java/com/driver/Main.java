98d93f1d34e1dbc0624a266037f339ae68adeeb8
kumar2812 <108868988+kumar2812@users.noreply.github.com>
2025-06-12 11:20:33 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,38 +1,23 @@
 package com.driver;
 
 public class Main {
-    // Task 1: Create a class Product inside Main class
-    static class Product {
-        // Task 3: Method to multiply two integers
-        public int product(int x, int y) {
-            return x * y;
-        }
+    public static void main(String[] args) {
+        Product p =new Product();
+        System.out.println( p.product(56,32));
+        System.out.println(p.product(23,14,16));
+        System.out.println(p.product(33.56,23.45));
 
-        // Task 4: Overloaded method to multiply three integers
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        // Task 5: Overloaded method to multiply two doubles
-        public double product(double x, double y) {
-            return x * y;
-        }
     }
 
-    public static void main(String[] args) {
-        // Task 2: Create an object of Product
-        Product p = new Product();
-
-        // Calling the method with two integers
-        int result1 = p.product(3, 4);
-        System.out.println("Product of 3 and 4: " + result1);
-
-        // Calling the overloaded method with three integers
-        int result2 = p.product(2, 3, 5);
-        System.out.println("Product of 2, 3, and 5: " + result2);
-
-        // Calling the overloaded method with two doubles
-        double result3 = p.product(2.5, 4.0);
-        System.out.println("Product of 2.5 and 4.0: " + result3);
+    public static class Product {
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
     }
 }
